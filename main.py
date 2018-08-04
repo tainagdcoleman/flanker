6d34f8b59c4d86858a42903b8e52dc2b2932f352
Taina Coleman <tainagdcoleman@gmail.com>
2018-08-04 15:20:44 -0700

    Implements projects with screens

--- main.py
+++ main.py
@@ -2,40 +2,64 @@ import kivy
 kivy.require('1.10.1')
 
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ListProperty
+
+import yaml
 
 info = {}
-game = 'start'
-app = None
+config = {}
 
-def update(key, value):
-    global info
-    info[key] = value
 
-def start_game(name):
-    global app, game
-    game = name
-    app.update()
+Builder.load_file('main.kv')
 
-class StartWidget(Widget):
-    pass
+sm = ScreenManager()
+
+class Start(Screen):
+    def on_enter(self):
+        global config
+        config = {}
+
+    def update(self, key, value):
+        global info
+        info[key] = value.encode('ascii')
+
+    def start_game(self, game):
+        global config
+        config = yaml.load(open('data/flanker.yml'))
+        sm.current = 'instruction'
+
+class Flanker(Screen):
+    def on_enter(self):
+        pass
+
+class Memory(Screen):
+    def on_enter(self):
+        pass
+
+class Instruction(Screen):
+    instructions = ListProperty([{'title': 'N/A'}])
+    
+    def on_enter(self):
+        global info, config
+        self.info = info 
+        self.instructions = config['instructions']
+        print(self.info)
+        print(self.instructions)
+
+    def next(self):
+        self.instructions = self.instructions[1:]
+
+
+sm.add_widget(Start(name='start'))
+sm.add_widget(Flanker(name='flanker'))
+sm.add_widget(Memory(name='memory'))
+sm.add_widget(Instruction(name = 'instruction'))
 
 class MyApp(App):
-    widget = None
-
-    def update(self): 
-        global game, info
-        self.widget.clear_widgets()
-        if game == 'start':
-            self.widget.add_widget(StartWidget())
-        else:
-            self.widget.add_widget(GridLayout())
-
     def build(self):
-        self.widget = StartWidget()
-        return self.widget
+        return sm
 
-app = MyApp()
-app.run()
+if __name__ == '__main__':
+    MyApp().run()
